3b4ccc50aac6a93eb7038bc6b1859ff5cceb4519
Nicola Riparelli <vegazrelli@gmail.com>
2019-01-11 14:25:57 +0100

    Update

--- routes/routes.js
+++ routes/routes.js
@@ -1,24 +1,6 @@
 var path       = require("path");
 var tools      = require('./../tools');
 
-// function score(req, res) {
-// 	var sentence = req.body.sentence;
-// 	var score    = 0;
-
-// 	if (sentence.length > 0) {
-// 		var words = sentence.split(" ");
-// 		words.forEach(function(word) {
-// 		    if (positiveWords.includes(word)) {
-// 		    	score += 10;
-// 		    } else if (negativeWords.includes(word)) {
-// 		    	score -= 10;
-// 		    }
-// 		});
-// 	}	
-
-//     res.json({score: score})
-// }
-
 var appRouter = function (app) {
   app.get("/", function(req, res) {
   	res.render('index');
